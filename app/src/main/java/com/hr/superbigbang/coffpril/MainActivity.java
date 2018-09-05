77167bb819495fbb224ef9cfde4584abf71c0ba4
SuperBigBang <superbigbang@yandex.ru>
2018-09-05 15:53:44 +0900

    last edits

--- app/src/main/java/com/hr/superbigbang/coffpril/MainActivity.java
+++ app/src/main/java/com/hr/superbigbang/coffpril/MainActivity.java
@@ -2,13 +2,10 @@ package com.hr.superbigbang.coffpril;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
--- app/src/test/java/com/hr/superbigbang/coffpril/ExampleUnitTest.java
+++ app/src/test/java/com/hr/superbigbang/coffpril/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.hr.superbigbang.coffpril;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
